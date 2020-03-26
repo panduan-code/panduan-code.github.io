f6ba9ce8b377346104b5c150472f797c571b6f7f
Panduan Code <50077727+PanduanCode@users.noreply.github.com>
2020-03-26 11:57:21 +0700

    Create apicorona.js

---
+++ apicorona.js
@@ -0,0 +1,5 @@
+ $.ajax({async: true, cache: true, url: "https://api.kawalcorona.com/indonesia/", success: function(result){
+             $("#positif").html(result[0].positif);
+             $("#sembuh").html(result[0].sembuh);
+             $("#meninggal").html(result[0].meninggal);
+            }});
